a286308ca87243f65142d44916ca4ad0187f85ab
Engin GÜRBÜZ <engurbuz@gmail.com>
2024-12-08 16:27:42 +0300

    react-codepen-app added

---
+++ Cohort/week12/react-codepen-app/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>© 2024 React CodePen App. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
